eac90212c284c1908382f5ffcac47d18b52bdbc8
abhijithabhijith1999 <abhijithabhijith1999@gmail.com>
2026-06-12 10:29:11 +0530

    feat: add RSA key and configure API proxy for remote backend communication

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api-proxy/:path*",
+        destination: "http://54.252.229.180/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
